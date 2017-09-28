de02ab589fb3fd892db1a6f067dac13154ebe8b9
MichailKlyushin <klyushin.mixail@gmail.com>
2017-09-29 03:51:52 +0500

    Prototype game
    Тестовая версия игры

---
+++ UnityGame/Assets/Scripts/PlauerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+[RequireComponent (typeof(PlayerMotor))]//подключить скрипт PlayMode
+public class PlauerController : MonoBehaviour {
+
+	[SerializeField]//можно редактировать переменную в Unity
+	private float speed = 5f;//скорость игрока
+	[SerializeField]
+	private float lookSpeed = 3f;
+
+	private PlayerMotor motor;
+
+	void Start (){
+		motor = GetComponent<PlayerMotor> ();
+	}
+
+	void Update(){
+		float xMov = Input.GetAxisRaw ("Horizontal");//движение по x
+		float zMov = Input.GetAxisRaw ("Vertical");//движение по z
+
+		Vector3 movHor = transform.right * xMov;
+		Vector3 movVer = transform.forward * zMov;
+
+		Vector3 velociti = (movHor + movVer).normalized * speed;//объединения движения
+
+		motor.Move (velociti);//применяем к объекту
+
+		float yRot = Input.GetAxisRaw ("Mouse X");//отслеживаем движение мыши
+		Vector3 rotation = new Vector3 (0f, yRot, 0f) * lookSpeed;
+
+		motor.Rotate (rotation);//применяем к объекту
+
+		float xRot = Input.GetAxisRaw ("Mouse Y");//отслеживаем движение мыши
+		Vector3 camRotation = new Vector3 (xRot, 0f, 0f) * lookSpeed;
+
+		motor.RotateCam (camRotation);//применяем к объекту
+	}
+}
---
+++ UnityGame/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]//подключить Rigidbody
+public class PlayerMotor : MonoBehaviour {
+
+	private Rigidbody rb;
+	[SerializeField]
+	private Camera cam;
+
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 rotation = Vector3.zero;
+	private Vector3 rotationCamera = Vector3.zero;
+
+	void Start(){
+		rb = GetComponent <Rigidbody> ();
+	}
+
+	public void Move (Vector3 _velocity){
+		velocity = _velocity;
+	}
+
+	public void Rotate (Vector3 _rotation){
+		rotation = _rotation;
+	}
+
+	public void RotateCam (Vector3 _rotationCam){
+		rotationCamera = _rotationCam;
+	}
+
+
+	void FixedUpdate (){
+		PerformMove ();
+		PerformRotation ();
+	}
+
+	void PerformMove (){
+		if (velocity != Vector3.zero)
+			rb.MovePosition (rb.position + velocity * Time.fixedDeltaTime);//если с толкнулся с объектом движение прекротилось
+	}
+
+	void PerformRotation (){
+		rb.MoveRotation (rb.rotation * Quaternion.Euler (rotation));//поворот мышью в право/лево
+		if (cam != null)
+			cam.transform.Rotate (-rotationCamera);
+	}
+}
